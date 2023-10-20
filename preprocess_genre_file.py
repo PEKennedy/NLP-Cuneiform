4033eff072634b801e90be33aaba68f946b23288
Peter Kennedy <pkenned5@unb.ca>
2023-10-20 13:16:58 -0300

    more notes for preprocessor

--- preprocess_genre_file.py
+++ preprocess_genre_file.py
@@ -4,6 +4,16 @@ import subprocess
 # or maybe modify Nuolenna to add in these markers in the unicode file it makes?
 # Maybe this is unnecessary and we should go by lines in a document?
 
+# A good place to split would be around here:
+# &P424664 = BAM 5, 498 (Rm 254)
+# atf: lang akk 
+# @tablet 
+# Splitting at the '=' would work, @tablet wouldn't necessarily work since there are a few inscriptions not on a tablet
+# perhaps at the '&' character, since a line like this can appear: &P396882 = BAM 512 = K 6894 -- Geer’s J 105
+
+
+
+
 # Run each document by Nuolenna
 
 # Combine the results into a single file
